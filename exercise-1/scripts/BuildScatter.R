05735f86fe95577ed06e521d43575db824f4b06f
jessica <jzhu2@uw.edu>
2017-11-14 14:01:55 -0800

    add scatter

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,13 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+library(plotly)
+
+buildScatterplot <- function(my.data, my.x, my.y, my.color, my.title, y.label, x.label){
+    plot <- plot_ly(data = my.data, x = ~my.x, y = ~my.y, mode="markers", type="scatter") %>% 
+      layout(title = 'Styled Scatter',
+             yaxis = list(zeroline = FALSE),
+             xaxis = list(zeroline = FALSE))
+  
+}
